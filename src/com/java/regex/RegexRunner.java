ba676d17d37efccd2733e1b5999637d15627a23b
drs317lakshami <devrajkumar317@gmail.com>
2023-02-06 22:37:26 +0530

    Added :User need to enter a valid mobile number

--- src/com/java/regex/RegexRunner.java
+++ src/com/java/regex/RegexRunner.java
@@ -5,19 +5,19 @@ import java.util.regex.Pattern;
 
 public class RegexRunner {
 
-	static boolean validate(String regex, String input) {
+	static boolean validate(String regex, String mobileNum) {
 
-		return Pattern.compile(regex).matcher(input).matches();
+		return Pattern.compile(regex).matcher(mobileNum).matches();
 
 	}
 
 	public static void main(String[] args) {
 
-		String emailPattern = "[a-zA-Z]+([_+-.][a-zA-Z])*[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$";
+		String mobilenumberPattern ="^(91)( )[6-9]{1}[0-9]{9}$";
 
-		String input = "abc@bl.co";
+		String mobileNum  = "91 7588808888";
 
-		 System.out.println(validate(emailPattern, input));
+		 System.out.println(validate(mobilenumberPattern, mobileNum));
 
 	}
 
