d4727888096590a7a4e34dcc5ea075026b290ab4
drs317lakshami <devrajkumar317@gmail.com>
2023-02-06 21:45:27 +0530

    Added :User need to enter a valid First Name

---
+++ src/com/java/regex/RegexRunner.java
@@ -0,0 +1,24 @@
+package com.java.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexRunner {
+
+	static boolean validate(String regex, String input) {
+
+		return Pattern.compile(regex).matcher(input).matches();
+
+	}
+
+	public static void main(String[] args) {
+
+		String firstNamePattern = "^[A-Z][a-zA-Z]{3,}$";
+
+		String input = "Devraj";
+
+		System.out.println(validate(firstNamePattern, input));
+
+	}
+
+}
