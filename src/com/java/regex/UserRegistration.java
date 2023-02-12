0ae53ad54add064735f133c73082f504e6221cb3
drs317lakshami <devrajkumar317@gmail.com>
2023-02-12 13:47:41 +0530

    Should clear all email samples provided separately

--- src/com/java/regex/UserRegistration.java
+++ src/com/java/regex/UserRegistration.java
@@ -1,20 +1,18 @@
 package com.java.regex;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
-	    public static void main(String[] args) {
-	        System.out.println("Welcome to Regular Expressions program to validate PIN code!");
-	        Validation validation=new Validation();
-	        validation.checkFirstName();
-	        validation.checkLastName();
-	        validation.checkEmail();
-	        validation.checkPhoneNumber();
-	        validation.checkPasswordRule1();
-	        validation.checkPasswordRule2();
-	        validation.checkPasswordRule3();
-	        validation.checkPasswordRule4();
-	    }
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Regular Expressions program to validate PIN code!");
+        Validation validation=new Validation();
+        validation.checkFirstName();
+        validation.checkLastName();
+        validation.checkEmail();
+        validation.checkPhoneNumber();
+        validation.checkPasswordRule1();
+        validation.checkPasswordRule2();
+        validation.checkPasswordRule3();
+        validation.checkPasswordRule4();
+        validation. checkEmail2();
+
+    }
+}
