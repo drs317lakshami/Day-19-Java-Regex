33e5b6b31cfd1474c0769cee02f4ab7b9b061c63
drs317lakshami <devrajkumar317@gmail.com>
2023-02-12 13:40:17 +0530

    validate exactly one special charactor

--- src/com/java/regex/UserRegistration.java
+++ src/com/java/regex/UserRegistration.java
@@ -5,84 +5,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
-	public static void main(String[] args) {
-		System.out.println("Regular Expression program to validate User Registration!!!");
-		UserRegistration userEntry = new UserRegistration();
-
-		userEntry.validateEntries();
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Regular Expressions program to validate PIN code!");
+	        Validation validation=new Validation();
+	        validation.checkFirstName();
+	        validation.checkLastName();
+	        validation.checkEmail();
+	        validation.checkPhoneNumber();
+	        validation.checkPasswordRule1();
+	        validation.checkPasswordRule2();
+	        validation.checkPasswordRule3();
+	        validation.checkPasswordRule4();
+	    }
 	}
-
-	public void validateEntries() {
-		System.out.println("Enter First Name :");
-		validateName(getUserInput());
-
-		System.out.println("Enter Last Name :");
-		validateName(getUserInput());
-
-		System.out.println("\nEnter Email Id : ");
-		validateEmail(getUserInput());
-
-		System.out.println("\nEnter Mobile Number : ");
-		validateMobileNumber(getUserInput());
-		
-		System.out.println("\nEnter Password : ");
-		validatePassword(getUserInput());
-
-	}
-
-	public String getUserInput() {
-		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
-	}
-	
-	private void validatePassword(String input) {
-		String password = "^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$";
-		
-		System.out.println("\nValidating Password...");
-		match(password, input);
-	}
-
-	public void validateMobileNumber(String input) {
-
-		String mobile = "^(91)( )[6-9]{1}[0-9]{9}$";
-
-		System.out.println("\nValidating Mobile Number...");
-		match(mobile, input);
-	}
-
-	public void validateEmail(String input) {
-
-		String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
-
-		System.out.println("\nValidating Email...");
-		match(email, input);
-	}
-
-	public void validateName(String input) {
-
-		String name = "^[A-Z][a-zA-Z]{3,}$";
-
-		Pattern pattern = Pattern.compile(name);
-		Matcher match = pattern.matcher(input);
-
-		System.out.println("Validating Name...");
-		printResult(match.matches(), input);
-
-	}
-
-	public void match(String regex, String input) {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher match = pattern.matcher(input);
-
-		printResult(match.matches(), input);
-	}
-
-	public void printResult(boolean isOk, String input) {
-		if (isOk)
-			System.out.println(" " + input + " is Valid");
-		else
-			System.out.println(" " + input + " is Invalid");
-	}
-
-}
---
+++ src/com/java/regex/Validation.java
@@ -0,0 +1,99 @@
+package com.java.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Validation {
+    Scanner sc = new Scanner(System.in);
+
+    public void checkFirstName() {
+        System.out.println("Enter First Name : ");
+        String firstName = sc.next();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", firstName)) {
+            System.out.println("First Name is Valid");
+        } else {
+            System.out.println("First Name is Invalid");
+        }
+    }
+
+    public void checkLastName() {
+        System.out.println("--------Check Last Name-------");
+        System.out.println("Enter Last Name : ");
+        String lastName = sc.next();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", lastName)) {
+            System.out.println("LastName is Valid");
+        } else {
+            System.out.println("LastName is Invalid");
+        }
+    }
+
+    public void checkEmail() {
+        System.out.println("------Check Email------");
+        System.out.println("Enter Email : ");
+        String email = sc.next();
+        if (Pattern.matches("^[a-zA-Z]{3,}[.]?[A-za-z0-9]{3,}[.]?[a-zA-Z]+[@]{1}[a-z]+[.]?[a-z]*", email)) {
+            System.out.println("Email is Valid");
+        } else {
+            System.out.println("Email is Invalid");
+        }
+    }
+
+    public void checkPhoneNumber() {
+        System.out.println("---------Check PhoneNumber-------");
+        System.out.println("Enter PhoneNumber : ");
+        String phoneNumber = sc.nextLine();
+        if (Pattern.matches("^[0-9]+\s[0-9]{10}", phoneNumber)) {
+            System.out.println("PhoneNumber is Valid");
+        } else {
+            System.out.println("PhoneNumber is Invalid");
+            checkPhoneNumber();
+        }
+    }
+
+    public void checkPasswordRule1() {
+        System.out.println("------Password Rule1------");
+        System.out.println("Enter Password : ");
+        String password = sc.nextLine();
+        if (Pattern.matches("^[a-zA-z]{8,}", password)) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+            checkPasswordRule1();
+        }
+    }
+
+    public void checkPasswordRule2() {
+        System.out.println("--------Password Rule2-------");
+        System.out.println("Enter Password : ");
+        String password = sc.nextLine();
+        if (Pattern.matches("^[A-z]+[A-Za-z]{8,}", password)) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+            checkPasswordRule1();
+        }
+    }
+
+    public void checkPasswordRule3() {
+        System.out.println("--------Password Rule3------");
+        System.out.println("Enter Password : ");
+        String password = sc.nextLine();
+        if (Pattern.matches("^[A-Z0-9]+[A-Za-z0-9]{8,}", password)) {
+            System.out.println("Password is Valid---!!");
+        } else {
+            System.out.println("Password is Invalid--!!");
+            checkPasswordRule1();
+        }
+    }
+    public void checkPasswordRule4() {
+        System.out.println("-------Password Rule3------");
+        System.out.println("Enter Password : ");
+        String password  = sc.nextLine();
+        if (Pattern.matches("^[A-Z]+[A-Za-z0-9]{7,}[!@#$%^&*()=]+",password)) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+            checkPasswordRule4();
+        }
+    }
+}
